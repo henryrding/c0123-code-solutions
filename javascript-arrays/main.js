a7ac58730e40c10583a2d45cfd61b7282221995d
Henry Ding <henryrding@yahoo.com>
2023-01-31 00:19:18 +0000

    Completed javascript-arrays exercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,20 @@
+var colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+var myString = 'America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.';
+console.log(myString);
+colors[2] = 'green';
+var myNewString = 'Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.';
+console.log(myNewString);
+console.log('value of colors:', colors);
+
+var students = ['Bishan', 'Brenda', 'Colin', 'Cynthia'];
+var numberOfStudents = students.length;
+var myNewerString = 'There are ' + numberOfStudents + ' students in the class.';
+console.log(myNewerString);
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+var myNewestString = 'The last student in the array is ' + lastStudent + '.';
+console.log(myNewestString);
+console.log('value of students:', students);
