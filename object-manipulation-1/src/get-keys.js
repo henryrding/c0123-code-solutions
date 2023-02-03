917a5436952a4b5569e02d23b9689a46e4af264a
Henry Ding <henryrding@yahoo.com>
2023-02-03 22:06:35 +0000

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-keys.js
@@ -0,0 +1,7 @@
+/* exported getKeys */
+function getKeys(object) {
+  var keys = [];
+  for (var x in object) {
+    keys.push(x);
+  } return keys;
+}
---
+++ object-manipulation-1/src/get-values.js
@@ -0,0 +1,7 @@
+/* exported getValues */
+function getValues(object) {
+  var values = [];
+  for (var x in object) {
+    values.push(object[x]);
+  } return values;
+}
