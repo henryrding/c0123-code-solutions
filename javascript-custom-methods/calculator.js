62e3850773dc3ea08d5dbe7724f2316fc88e97ed
Henry Ding <henryrding@yahoo.com>
2023-02-20 17:29:45 +0000

    Completed javascript-custom-methods exercise.

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,27 @@
+/* exported calculator */
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var sum = numbers[0];
+    for (var i = 1; i < numbers.length; i++) {
+      sum += numbers[i];
+    } return sum;
+  },
+  getAverage: function (numbers) {
+    return this.sumAll(numbers) / numbers.length;
+  }
+};
