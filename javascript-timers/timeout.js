94a2c351aea3573c8451fc54c7715c2cb87310f1
Henry Ding <henryrding@yahoo.com>
2023-02-22 17:50:26 +0000

    Deleted unused code, stored setInderval return value in a variable, and used that variable as parameter for clearInterval.

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,9 +1,5 @@
 var $message = document.querySelector('.message');
 
-// setTimeout(() => {
-//   $message.textContent = 'Hello There';
-// }, 2000);
-
 setTimeout(function () {
   $message.textContent = 'Hello There';
 }, 2000);
