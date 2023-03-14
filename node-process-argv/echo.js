093ab5a86ec0a1eae23b94f1498c5e7445427b32
Henry Ding <henryrding@yahoo.com>
2023-03-14 23:03:43 +0000

    Completed node-process-argv exercise.

---
+++ node-process-argv/echo.js
@@ -0,0 +1,7 @@
+// not sure if user can type more than one message?
+//
+// const [, , ...message] = process.argv;
+// console.log(message.join(' '));
+
+const [, , message] = process.argv;
+console.log(message);
