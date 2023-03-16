f52ae447c9763f7caf83ea7c964816826a01f098
Henry Ding <henryrding@yahoo.com>
2023-03-16 06:17:58 +0000

    Added other test cases to make sure function works properly.

--- array-map/map.js
+++ array-map/map.js
@@ -7,5 +7,18 @@ function map(array, transform) {
 }
 
 // test cases
-// console.log(map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (number) => number * 2));
-// console.log(map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (number) => `$${number.toFixed(2)}`));
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const languages = [
+//   'Hypertext Markup Language',
+//   'Cascading Style Sheets',
+//   'ECMAScript',
+//   'PHP Hypertext Preprocessor',
+//   'Structured Query Language'
+// ];
+
+// console.log(map(numbers, (number) => number * 2));
+// console.log(map(numbers, (number) => `$${number.toFixed(2)}`));
+// console.log(map(languages, (language) => language.toUpperCase()));
+// console.log(map(languages, (language) => language[0]));
