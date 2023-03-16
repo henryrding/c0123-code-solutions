7fcea316c4acd673d210baa71837319816cc15b1
Henry Ding <henryrding@yahoo.com>
2023-03-16 06:14:09 +0000

    Added other test cases to make sure function works properly.

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -9,5 +9,22 @@ function filter(array, predicate) {
 }
 
 // test cases
-// console.log(filter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (number) => !(number % 2)));
-// console.log(filter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (number) => number > 5));
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const names = [
+//   'Ada',
+//   'Hedy',
+//   'Jean',
+//   'Grace',
+//   'Evelyn',
+//   'Joan',
+//   'Elizabeth',
+//   'Janese',
+//   'Donna'
+// ];
+
+// console.log(filter(numbers, (number) => !(number % 2)));
+// console.log(filter(numbers, (number) => number > 5));
+// console.log(filter(names, (name) => name.startsWith('E')));
+// console.log(filter(names, (name) => name.includes('D') || name.includes('d')));
