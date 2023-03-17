a98ecca8b4902b2fc0c7cc64f0b5ad23cae80427
Henry Ding <henryrding@yahoo.com>
2023-03-17 23:27:55 +0000

    Fixed write function arguments in update.js.

--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -5,7 +5,7 @@ export default async function updateNote(id, notes) {
   const dataCopyObject = await read();
   if (dataCopyObject.notes[id]) {
     dataCopyObject.notes[id] = notes.join(' ');
-    await write('data.json', JSON.stringify(dataCopyObject, null, 2));
+    await write(dataCopyObject);
   } else {
     throw new Error(`Note ${id} doesn't exist!`);
   }
