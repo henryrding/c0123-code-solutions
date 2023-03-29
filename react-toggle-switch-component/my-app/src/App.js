f1e6bf79321b3d8fab5ae57ea760c8dfac6101e7
Henry Ding <henryrding@yahoo.com>
2023-03-29 20:36:00 +0000

    Completed react-toggle-switch-component exercise.

---
+++ react-toggle-switch-component/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ToggleSwitch from './ToggleSwitch';
+
+function App() {
+  return (
+    <div className="Row">
+      <div className="Column">
+        <ToggleSwitch />
+      </div>
+    </div>
+  );
+}
+
+export default App;
