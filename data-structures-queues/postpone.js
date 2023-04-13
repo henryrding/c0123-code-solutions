34337552bef526546c30673c9757c003f5bc48cd
Henry Ding <henryrding@yahoo.com>
2023-04-13 18:13:38 +0000

    Changed postpone.js to be more readable.

--- data-structures-queues/postpone.js
+++ data-structures-queues/postpone.js
@@ -1,7 +1,6 @@
 export default function postpone(queue) {
-  // if (queue.peek() !== undefined) {
-  //   const temp = queue.dequeue();
-  //   queue.enqueue(temp);
-  // }
-  queue.peek() !== undefined && queue.enqueue(queue.dequeue());
+  if (queue.peek() !== undefined) {
+    queue.enqueue(queue.dequeue());
+  }
+  // queue.peek() !== undefined && queue.enqueue(queue.dequeue());
 }
